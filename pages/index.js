70b7a3c273d9dd04d1bd8b8e20c0799439504b73
Alex Santos <devalexsantos@gmail.com>
2021-09-20 00:54:31 -0300

    Debbuging to vercel

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
-
 import Header from './components/Header';
-import Navbar from './components/Navbar';
 import FeaturedProducts from './components/FeaturedProducts';
 import LastProducts from './components/LastProducts';
 import { getFeaturedProducts, getLastProducts } from './dato-cms';
-import styled from 'styled-components';
+import Script from 'next/script'
 
 const Home = ({featured, last}) => (
     <>
@@ -27,7 +23,7 @@ const Home = ({featured, last}) => (
       <footer>
         <span>iMall071</span>
       </footer>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossOrigin="anonymous"></script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossOrigin="anonymous"></Script>
     </>
   );
 
