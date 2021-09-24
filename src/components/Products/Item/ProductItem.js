ffbda1860c40618a011509ae48af4ff081568783
Alex Santos <devalexsantos@gmail.com>
2021-09-24 19:15:04 -0300

    adicionando NProgress e descrição da página de produtos

--- src/components/Products/Item/ProductItem.js
+++ src/components/Products/Item/ProductItem.js
@@ -32,7 +32,7 @@ export default function ProductItem(props) {
             <div className="content-info-single p-4 col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7">
                 <div className={styles.infoContentSingle}>
                 <h1>{props.title}</h1>
-                <p className="pt-2 rounded-12">{props.shortDesc}</p>
+                <p className="pt-2 rounded-12" dangerouslySetInnerHTML={{ __html: props.shortDesc }}></p>
                 <span className={styles.productSinglePrice}>{changePrice(props.price)}</span><br/>
                 <span className="pt-5">Forma de pagamento:</span>
                 <span><a className="mt-3 d-block btn btn-success w-auto" href="#" role="button">Pedir no WhatsApp</a></span>
